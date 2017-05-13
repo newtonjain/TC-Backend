932e849afa156ff23d0ccaa46cd17f78a19cd97c
Arin Ray <arinray92@gmail.com>
2017-05-13 18:42:38 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+var MongoClient = require('mongodb').MongoClient;
+const PORT = 3000;
+const VERSION = "/api/v1"
+var admin = require("firebase-admin");
+var bodyParser = require('body-parser');
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gentle-keyword-90702.firebaseio.com"
+});
+
+// Get a database reference to our blog
+var db = admin.database();
+var ref = db.ref("server/fitness");
+
+// var uri = "mongodb://kay:disrupt17@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+// MongoClient.connect(uri, function(err, db) {
+//       if (err) throw err
+//   //db.close();
+//     app.listen(PORT, function() {
+//         console.log('listening on '+PORT)
+//     });
+// });
+
+var express = require('express')
+ 
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Get user
+app.get('/users/:user_id', function(req, res) {
+console.log(req.params.user_id);
+    var usersRef = ref.child("users/"+req.params.user_id);
+    usersRef.on("value", function(snapshot) {
+        console.log(snapshot.val());
+        res.send({success:true, user: snapshot.val()})
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+})
+
+// Update steps
+app.post('/users/:user_id/steps', (req, res) => {
+    var usersRef = ref.child("users/"+req.params.user_id);
+    console.log("REQ BODY:", req.body);
+    var response = {}
+    // Set next level
+    if(req.body.steps < 1000){
+        response.level = 1
+        response.next_level = { 
+            level: 2, 
+            steps_required: 2000 
+        }
+    }else{
+        response.level = Math.floor(req.body.steps / 1000) + 1;
+        response.next_level = {
+            level: response.level + 1,
+            steps_required: (1000 * (response.level + 1)),
+        }
+    }
+    usersRef.update({steps: req.body.steps, level: response.level});
+    res.status(201);
+    res.send(response);
+})
+
+app.listen(PORT, function() {
+    console.log('listening on '+PORT)
+});
